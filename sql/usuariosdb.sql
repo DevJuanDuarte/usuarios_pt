fa7faa47da4affaabbaf3f41db8e97b0d3fc15f9
Juan Duarte <juan.devcontact@gmail.com>
2024-11-06 15:09:49 -0500

    Actualizando script de base de datos

--- sql/usuariosdb.sql
+++ sql/usuariosdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 06, 2024 at 06:41 AM
+-- Generation Time: Nov 06, 2024 at 08:08 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.2.23
 
@@ -36,13 +36,6 @@ CREATE TABLE `posts` (
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `posts`
---
-
-INSERT INTO `posts` (`id`, `title`, `content`, `userid`, `created_at`, `updated_at`) VALUES
-(3, 'Mi primer post', 'Este es el contenido de mi primer post en la plataforma.', 25, '2024-11-06 06:30:42', '2024-11-06 06:30:42');
-
 -- --------------------------------------------------------
 
 --
@@ -57,13 +50,6 @@ CREATE TABLE `users` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `updated_at`) VALUES
-(25, 'test', 'test@gmail.com', '$2y$10$ZO43m/pJyl.d8PvKgULtGeayvcce39e9R.fnmfZr3giDmWp0Wa6pa', '2024-11-06 06:30:37', '2024-11-06 06:30:37');
 
 --
 -- Indexes for dumped tables
@@ -92,13 +78,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- Constraints for dumped tables
